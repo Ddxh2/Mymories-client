b4f6633f464afe4cee2c2f3ed6ea602ec0f2d923
Danny He <dannyhe@hotmail.co.uk>
2021-08-31 16:03:33 +0100

    feature/rsa added rsa encryption before sending passwords to server for login or signup

--- src/reducers/users.js
+++ src/reducers/users.js
@@ -1,10 +1,12 @@
 import { ACTION_TYPES } from "../constants/actionTypes";
 
-const reducer = (loggedIn = false, action) => {
+const reducer = (loggedIn = { success: null, type: null }, action) => {
   switch (action.type) {
     case ACTION_TYPES.LOG_IN:
-    case ACTION_TYPES.CREATE_USER:
       return action.payload;
+
+    case ACTION_TYPES.CREATE_USER:
+      return { success: action.payload };
     default:
       return loggedIn;
   }
