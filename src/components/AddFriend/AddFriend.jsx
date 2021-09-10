8f5796da857cb533af8996f274b343f415b7f26f
Danny He <dannyhe@hotmail.co.uk>
2021-09-10 17:09:16 +0100

    changed AddFriend styling for consistency

--- src/components/AddFriend/AddFriend.jsx
+++ src/components/AddFriend/AddFriend.jsx
@@ -2,10 +2,17 @@ import React, { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
 
 import { getMyFriends, addFriend, deleteFriend } from "../../api";
+import PersonAddIcon from "@material-ui/icons/PersonAdd";
+import PersonAddDisabledIcon from "@material-ui/icons/PersonAddDisabled";
 
 import "./AddFriend.css";
 
-const AddFriend = ({ userId, onAdd, onRemove }) => {
+const AddFriend = ({
+  className = "",
+  userId,
+  onAdd = () => {},
+  onRemove = () => {},
+}) => {
   const loggedInUserId = useSelector((state) => state.loggedInUser.id);
   const [existingFriend, setExistingFriend] = useState(undefined);
 
@@ -28,7 +35,7 @@ const AddFriend = ({ userId, onAdd, onRemove }) => {
   };
 
   useEffect(() => {
-    if (!!loggedInUserId && !!userId) {
+    if (!!loggedInUserId && !!userId && loggedInUserId !== userId) {
       getMyFriends(loggedInUserId).then(({ data }) => {
         const existingFriend = data.find(
           ({ userId1, userId2 }) =>
@@ -44,10 +51,20 @@ const AddFriend = ({ userId, onAdd, onRemove }) => {
   return existingFriend === undefined ? null : (
     <button
       className={`friendship__button ${
-        !!existingFriend ? "unfriend" : "addFriend"
-      }`}
+        existingFriend === undefined
+          ? "friendship__button__hidden"
+          : !!existingFriend
+          ? "unfriend"
+          : "addFriend"
+      } ${className}`}
       onClick={onClick}
     >
+      {!!existingFriend ? (
+        <PersonAddDisabledIcon fontSize='inherit' />
+      ) : (
+        <PersonAddIcon fontSize='inherit' />
+      )}
+      &nbsp;
       {!!existingFriend ? "Unfriend" : "Add Friend"}
     </button>
   );
